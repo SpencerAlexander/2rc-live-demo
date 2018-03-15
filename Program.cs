bf1e8528b72ff49fab835c04457aed7336c675a1
Spencer Alexander <spencer.alexander@WS2393.local>
2018-03-14 18:08:35 -0700

    initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _2rc_awaken_example
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
